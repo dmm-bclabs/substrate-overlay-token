28a5a396a706d6b9e300007a4cc516d27322f675
Nagato Kasaki <kasaki-nagato@dmm.com>
2019-05-28 18:50:35 +0900

    impl init fn

--- runtime/src/token.rs
+++ runtime/src/token.rs
@@ -8,7 +8,8 @@
 /// For more guidance on Substrate modules, see the example module
 /// https://github.com/paritytech/substrate/blob/master/srml/example/src/lib.rs
 
-use support::{decl_module, decl_storage, decl_event, StorageValue, dispatch::Result};
+use rstd::prelude::Vec;
+use support::{decl_module, decl_storage, decl_event, StorageValue, dispatch::Result, ensure, StorageMap};
 use system::ensure_signed;
 
 /// The module's configuration trait.
@@ -22,10 +23,18 @@ pub trait Trait: system::Trait {
 // This module's storage items.
 decl_storage! {
 	trait Store for Module<T: Trait> as token {
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(something): Option<u32>;
+
+		Init get(is_init): bool;
+
+		Owner get(owner): T::AccountId;
+
+		TotalSupply get(total_supply): u128;
+
+		Name get(name): Vec<u8>;
+		Ticker get(ticker): Vec<u8>;
+
+		BalanceOf get(balance_of): map T::AccountId => u128;
+		Allowance get(allowance): map (T::AccountId, T::AccountId) => u128;
 	}
 }
 
@@ -37,19 +46,16 @@ decl_module! {
 		// this is needed only if you are using events in your module
 		fn deposit_event<T>() = default;
 
-		// Just a dummy entry point.
-		// function that can be called by the external world as an extrinsics call
-		// takes a parameter of the type `AccountId`, stores it and emits an event
-		pub fn do_something(origin, something: u32) -> Result {
-			// TODO: You only need this if you want to check it was signed.
-			let who = ensure_signed(origin)?;
+		fn init(origin, total_supply: u128) -> Result {
+			let sender = ensure_signed(origin)?;
 
-			// TODO: Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			<Something<T>>::put(something);
+			ensure!(Self::is_init() == false, "Already initialized.");
 
-			// here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			<TotalSupply<T>>::put(total_supply);
+			<BalanceOf<T>>::insert(sender.clone(), total_supply);
+			<Owner<T>>::put(sender.clone());
+			<Init<T>>::put(true);
+
 			Ok(())
 		}
 	}
