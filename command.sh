5eb8fa4e9a34b0168cb4b9515b87fd1b1152a8c4
wshino <wshinohara@gmail.com>
2019-07-09 08:59:36 +0000

    docker-compose files

---
+++ command.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+help () {
+  echo "It receive only below argument." 1>&2
+  echo "----" 1>&2
+  echo "build" 1>&2
+  echo "log parent|child|bridge" 1>&2
+  echo "run" 1>&2
+  echo "stop" 1>&2
+  exit 1
+}
+
+container_name_error () {
+  echo "Container name only receive below name." 1>&2
+  echo "----" 1>&2
+  echo "parent" 1>&2
+  echo "child" 1>&2
+  echo "bridge" 1>&2
+  exit 1
+}
+if [ $# -gt 2 ]; then
+    help
+fi
+
+if [ $1 = "build" ]
+then
+    cd substrate-overlay-token \
+    && ./scripts/build.sh \
+    && cargo build --release
+elif [ $1 = "run" ]
+then
+    cp substrate-overlay-token/docker .
+    sudo docker-compose up -d
+elif [ $1 = "log" ]
+then
+    if [ $2 = "parent" ] || [ $2 = "child" ] || [ $2 = "bridge" ]
+    then
+        sudo docker-compose logs -f --tail=10 $2
+    else
+        container_name_error
+    fi
+elif [ $1 = "stop" ]
+then
+    sudo docker-compose stop
+else 
+    help
+fi
+
